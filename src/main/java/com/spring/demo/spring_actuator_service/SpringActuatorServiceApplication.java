d92ae677103707d2961eac6abac8c0f0fca82a20
Shruti Kulkarni <166386268+kulkarnishruti4@users.noreply.github.com>
2024-07-18 15:10:55 +0530

    Initial Commit

---
+++ src/main/java/com/spring/demo/spring_actuator_service/SpringActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.demo.spring_actuator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/demo/spring_actuator_service/SpringActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demo.spring_actuator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
